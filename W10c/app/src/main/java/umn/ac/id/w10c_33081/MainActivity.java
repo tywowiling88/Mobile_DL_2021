ef711c067cba1f9d201eebd79307a9a3ec56735d
Teofilus <teofilus.yohanes@student.umn.ac.id>
2021-04-19 21:09:49 +0700

    Service

--- W10c/app/src/main/java/umn/ac/id/w10c_33081/MainActivity.java
+++ W10c/app/src/main/java/umn/ac/id/w10c_33081/MainActivity.java
@@ -2,12 +2,35 @@ package umn.ac.id.w10c_33081;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    CustomBoundService customBoundService;
+    boolean isBound = false;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            CustomBoundService.CustomLocalBinder binder =
+                    (CustomBoundService.CustomLocalBinder) service;
+            customBoundService = binder.getService();
+            isBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            isBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +47,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CustomService.class);
                 startService(intent);
+            }
+        });
+
+        Intent intent2 = new Intent(this, CustomBoundService.class);
+        bindService(intent2, serviceConnection, Context.BIND_AUTO_CREATE);
+
+        Button btnShowTime = findViewById(R.id.btnShowTime);
+        btnShowTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentTime =customBoundService.getCurrentTime();
+                Toast.makeText(getApplicationContext(), currentTime, Toast.LENGTH_SHORT).show();
             }
         });
     }
